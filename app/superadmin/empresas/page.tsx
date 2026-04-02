fdb3cc8a9f497f4c45d7094bbbc060165f0c08bf
Franchu <franciscobaezasanchez13@gmail.com>
2026-04-02 09:56:03 +0200

    Creacion de menu de gestion de empresas

---
+++ app/superadmin/empresas/page.tsx
@@ -0,0 +1,6 @@
+// src/app/superadmin/empresas/page.tsx
+import GestionEmpresas from "@/components/pages/GestionEmpresas";
+
+export default function SuperAdminEmpresasPage() {
+  return <GestionEmpresas />;
+}
